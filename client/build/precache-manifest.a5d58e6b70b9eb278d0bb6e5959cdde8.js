914426dfbb8758ad9e2590f9343fa034e3e06130
sayeed09 <sayeedshaikh09@gmail.com>
2019-09-02 18:27:50 +0530

    added build

---
+++ client/build/precache-manifest.a5d58e6b70b9eb278d0bb6e5959cdde8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeed00dbe74470ed034f87e6e4bfaa74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85f59dbdcff62e97ea9c",
+    "url": "/static/js/2.5868d4f3.chunk.js"
+  },
+  {
+    "revision": "1395ad6f1ace4796d5c2",
+    "url": "/static/js/main.11fca01c.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
